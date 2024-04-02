dc0e9a2c6b18d0555b8045b2868db510bfba3f86
tpluscode <tpluscode@users.noreply.github.com>
2024-04-02 12:50:04 +0200

    chore: updates

---
+++ .changeset/early-ravens-press.md
@@ -0,0 +1,5 @@
+---
+"@hydrofoil/resource-store": minor
+---
+
+Remove dependency on `@zazuko/env`
--- index.ts
+++ index.ts
@@ -1,15 +1,11 @@
-/**
- * @packageDocumentation
- * @module @hydrofoil/resource-store
- */
-
-import { DatasetCore, NamedNode, Term } from 'rdf-js'
+import type { DatasetCore, NamedNode, Term, DataFactory, DatasetCoreFactory } from '@rdfjs/types'
 import type { GraphPointer } from 'clownface'
-import { StreamClient } from 'sparql-http-client/StreamClient'
+import type { StreamClient } from 'sparql-http-client/StreamClient.js'
 import { ASK, CONSTRUCT, INSERT } from '@tpluscode/sparql-builder'
-import $rdf from '@zazuko/env'
 import { sparql } from '@tpluscode/rdf-string'
 import fromStream from 'rdf-dataset-ext/fromStream.js'
+import type { Environment } from '@rdfjs/environment/Environment.js'
+import type ClownfaceFactory from 'clownface/Factory.js'
 
 /**
  * Provides functions to work with individual resources.
@@ -17,10 +13,10 @@ import fromStream from 'rdf-dataset-ext/fromStream.js'
  * Implementors should take care to retrieve and manipulate only resource's "own" triples,
  * that is, avoid retrieving inferred statements or statements from other resources' representations.
  */
-export interface ResourceStore {
+export interface ResourceStore<D extends DatasetCore = DatasetCore> {
   exists(term: Term): Promise<boolean>
 
-  load(term: Term): Promise<GraphPointer<NamedNode, DatasetCore>>
+  load(term: Term): Promise<GraphPointer<NamedNode, D>>
 
   save(resource: GraphPointer<NamedNode>): Promise<void>
 
@@ -37,25 +33,26 @@ function assertNamedNode(term: Term): asserts term is NamedNode {
  * Default implementation of {@see ResourceStore}, which keeps each resource
  * is its own named graph.
  */
-export class ResourcePerGraphStore implements ResourceStore {
-  constructor(private client: StreamClient) {
+export class ResourcePerGraphStore<D extends DatasetCore> implements ResourceStore<D> {
+  // eslint-disable-next-line  @typescript-eslint/no-explicit-any
+  constructor(private client: StreamClient, private env: Environment<DataFactory | DatasetCoreFactory<any, any, D> | ClownfaceFactory>) {
   }
 
   exists(term: Term): Promise<boolean> {
     assertNamedNode(term)
 
-    return ASK`?s ?p ?o`.FROM(term).execute(this.client.query)
+    return ASK`?s ?p ?o`.FROM(term).execute(this.client)
   }
 
-  async load(term: Term): Promise<GraphPointer<NamedNode>> {
+  async load(term: Term): Promise<GraphPointer<NamedNode, D>> {
     assertNamedNode(term)
 
-    const dataset = await fromStream($rdf.dataset(), await CONSTRUCT`?s ?p ?o`
+    const dataset = await fromStream(this.env.dataset(), CONSTRUCT`?s ?p ?o`
       .FROM(term)
       .WHERE`?s ?p ?o`
-      .execute(this.client.query))
+      .execute(this.client))
 
-    return $rdf.clownface({ dataset, term })
+    return this.env.clownface({ dataset, term })
   }
 
   async save(resource: GraphPointer<NamedNode>): Promise<void> {
